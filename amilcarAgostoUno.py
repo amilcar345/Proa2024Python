0afb5635c89d8faacd229f5dcc5df8155202193d
amilcar345 <163875754+amilcar345@users.noreply.github.com>
2024-08-19 11:39:46 -0300

    Add files via upload

--- amilcarAgostoUno.py
+++ amilcarAgostoUno.py
@@ -21,3 +21,21 @@ def gestionar_tareas():
                     print(f"{i + 1}. {tareas[i]}")
             else:
                 print("\nNo hay tareas pendientes.\n")
+        elif opcion == "3":
+            if tareas:
+                numTarea = input("Introduce el número de la tarea que completaste: ")
+                if numTarea.isdigit() and 1 <= int(numTarea) <= len(tareas):
+                    tareaCompletada = tareas.pop(int(numTarea) - 1)
+                    print(f"Tarea '{tareaCompletada}' marcada como completada.\n")
+                else:
+                    print("Número de tarea no válido.")
+            else:
+                print("\nNo hay tareas para marcar como completadas.\n")
+        
+        elif opcion == "4":
+            print("Saliendo del programa.")
+            break
+        
+        else:
+            print("Opción no válida. Por favor, elige una opción entre 1 y 4.")
+gestionar_tareas()
